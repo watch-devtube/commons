5ce3852053f928056bfe7fb7049ce9d5c9d2aa75
Andrey Adamovich <andrey@aestasit.com>
2018-07-14 14:40:51 +0300

    export google classes

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@
 export { Dns, dnsCache } from './lib/Dns'
 export { default as Fastr } from './lib/Fastr'
 export { Logger } from './lib/Logger'
+export { GoogleBucket } from './lib/GoogleBucket'
+export { GoogleSubscriber } from './lib/GoogleSubscriber'
