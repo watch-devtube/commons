a6302dfcf9f863de61b2ca1cfebe1dbfdc2d4b8d
Andrey Adamovich <andrey@aestasit.com>
2018-08-12 12:03:06 +0300

    extended interface of GoogleBucket

--- lib/GoogleBucket.ts
+++ lib/GoogleBucket.ts
@@ -13,6 +13,8 @@ export class GoogleBucket {
     this.bucket = new Storage().bucket(bucket)
   }
 
+  name = () => this.bucket.name
+  
   count = (): Promise<number> => this.bucket.getFiles().then(files => files.map(a => a.length).reduce((a, b, i, arr) => a + b))
 
   files = (): Promise<File[]> => this.bucket.getFiles().then(files => _.flatten(files))
@@ -20,6 +22,11 @@ export class GoogleBucket {
   deleteAll = (): Promise<void> => this.bucket.deleteFiles()
   
   save = (path: string, data: string | Buffer): Promise<void> => this.bucket.file(path).save(data)
+
+  makePublic = (path: string): Promise<void> => this.bucket.file(path).acl.add({ entity: 'allUsers', role: 'READER' }).then((response) => {
+    let acl = response[0]
+    let apiResponse = response[1]
+  })
 
   get = (path: string): Promise<string> => this.bucket.file(path).download().then(bs => Buffer.concat(bs).toString())
 
