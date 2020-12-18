57fc27f6ef89007259d1d888c43d95b1a4cdba32
Eduards Sizovs <eduards@devternity.com>
2020-12-19 01:13:35 +0200

    remove unnecessary type exports

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 
 export { Dns, dnsCache } from './lib/Dns'
 export { default as Fastr, FastrOptions, SerializedIndex } from './lib/Fastr'
-export { Video, Speaker, Tag, Channel } from './lib/Fastr'
+export { Video, Speaker } from './lib/Fastr'
 export { Logger } from './lib/Logger'
 export { GoogleBucket } from './lib/GoogleBucket'
 export { GoogleSubscriber } from './lib/GoogleSubscriber'
